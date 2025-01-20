7a761fb4070be0aa912f2f7ef86f54cdb5311f6b
NikitaSec <santianogames@gmial.com>
2025-01-20 18:06:26 +0300

    first commit

---
+++ VideoSummarizer/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace VideoSummarizer.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
